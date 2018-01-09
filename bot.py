051475f0a1b8108a84cb87f99c3b98ff9df86b42
Thomas RETY <thomas.rety@epitech.eu>
2018-01-09 09:25:03 +0100

    truc

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ async def on_message(message):
         owner = message.server.owner
         owner = owner.name
         verification_level = str(message.server.verification_level)
-        afk_timeout = message.server.afk_timeout
+        afk_timeout = str(message.server.afk_timeout)
         region = str(message.server.region)
         lroles = message.server.roles
         name = message.server.name
